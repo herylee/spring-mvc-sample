d77c8601ae4febd9220ed29d918edca92d44001f
joshuashaw <joshuashaw@foxmail.com>
2016-04-18 17:08:23 +0800

    added security module and modified README

--- src/main/java/com/joshua/controller/EmployeeController.java
+++ src/main/java/com/joshua/controller/EmployeeController.java
@@ -1,11 +1,14 @@
 package com.joshua.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.joshua.service.EmployeeService;
 
@@ -13,18 +16,17 @@ import com.joshua.service.EmployeeService;
 @RequestMapping("/employee")
 public class EmployeeController {
     @Autowired
-    EmployeeService manager;
+    EmployeeService service;
  
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     public String getAllEmployees(Model model) {
-        model.addAttribute("employees", manager.getAllEmployees());
+        model.addAttribute("employees", service.getAllEmployees());
         return "employeesListDisplay";
     }
     
     @RequestMapping(value = "/view/{id}", method = RequestMethod.GET)
-    public String get(@PathVariable("id") long id, Model model) {
-    	model.addAttribute("employee", manager.getEmployeeById(id));
-        return "getEmployeeById";
-
+    public ModelAndView get(@PathVariable("id") long id,HttpServletRequest request, Model model) {
+    	return new ModelAndView("getEmployeeById")
+    			  .addObject("employee", service.getEmployeeById(id));
     }
 }
